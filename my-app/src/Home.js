e5d245784755aada412dae216edb613938021717
Lauren <halpertln@gmail.com>
2023-04-07 11:58:59 -0600

    redo Home

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -3,10 +3,10 @@ import React from "react";
 function Home() {
     return(
         <div id="home">
-            <img src="https://thumbs.gfycat.com/GlitteringAcceptableEchidna-size_restricted.gif" alt="library"></img>
-            <h2>Welcome to Book Trackr!</h2>
+            <h2>Welcome!</h2>
             <p>We love books here, and invite you to use our site to keep track of books you'd like to read and track the books you've read.</p>
             <p>Happy reading!</p>
+            <img src="https://thumbs.gfycat.com/GlitteringAcceptableEchidna-size_restricted.gif" alt="library"></img>
         </div>
     )
 }
